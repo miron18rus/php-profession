1a295c12de5b93ea5492210fc1ae09405faa6bc0
miron18rus <61833398+miron18rus@users.noreply.github.com>
2022-05-31 15:29:27 +0400

    update homework

--- public/index.php
+++ public/index.php
@@ -7,7 +7,7 @@ include '../engine/Autoload.php';
 
 spl_autoload_register([new Autoload(), 'loadClass']);
 
-$product = new Products('Пицца', 'Пепперони', 300);
+$product = new Products('Пицца', 'Пепперони', 300); 
 $user = new Users('User', '123qwe');
 var_dump($product->insert());
 var_dump($product->delete());
