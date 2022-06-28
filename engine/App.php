2f422f09fb90856ee1f9bebe92878dde4b3b1e58
miron18rus <61833398+miron18rus@users.noreply.github.com>
2022-06-28 19:44:29 +0400

    add class App

---
+++ engine/App.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace app\engine;
+
+
+use app\models\repositories\BasketRepository;
+use app\models\repositories\ProductRepository;
+use app\models\repositories\UserRepository;
+
+/**
+ * Class App
+ * @property Request $request
+ * @property BasketRepository $basketRepository
+ * @property UserRepository $userRepository
+ * @property ProductRepository $productRepository
+ * @property Db $db
+ */
+
+class App
+{
+
+}
